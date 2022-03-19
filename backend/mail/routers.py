78345cc28c13a28c1542035c42766d8d2ff6950f
Vasylchenko <vasyaliske@gmail.com>
2022-03-19 20:03:27 +0100

    Fixed debug route

--- backend/mail/routers.py
+++ backend/mail/routers.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 
+from config import settings
 from mail import views
 from mail import schemas
 
 router = APIRouter(prefix='/mail', tags=["mail"])
 
-router.add_api_route(
-    "/send_test_email",
-    views.send_test_email_api,
-    methods=["POST"],
-    response_model=schemas.SendTestEmailResponse
-)
+if settings.DEBUG:
+    router.add_api_route(
+        "/send_test_email",
+        views.send_test_email_api,
+        methods=["POST"],
+        response_model=schemas.SendTestEmailResponse
+    )
